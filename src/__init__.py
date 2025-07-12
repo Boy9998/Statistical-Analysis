b2002d049725b398f0c491ab04414dde9906a8e8
Boy9998 <1763661350@qq.com>
2025-07-13 06:21:27 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
+# src/__init__.py
 
+# 导入关键函数和类
+from .utils import fetch_historical_data, zodiac_mapping, send_dingtalk, send_email
+from .analysis import LotteryAnalyzer
+
+__all__ = [
+    'fetch_historical_data',
+    'zodiac_mapping',
+    'send_dingtalk',
+    'send_email',
+    'LotteryAnalyzer'
+]
